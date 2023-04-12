e851e6c1d229a14be6004d8c558e4e5c79c1753c
hp <hp@LAPTOP-VKHF4497>
2023-04-12 11:17:45 +0530

    first commit

---
+++ src/com/zxc/Calculation.java
@@ -0,0 +1,30 @@
+package com.zxc;
+
+public class Calculation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        Cal c=new Cal(20,30);
+        c.add();
+        c.sub();
+	}
+
+}
+class Cal
+{  
+	private int a;
+	private int b;
+	Cal(int a,int b)
+	{
+		this.a=a;
+		this.b=b;
+	}
+	 void add()
+	{
+		System.out.println(a+b);
+	}
+	 void sub()
+	{
+		System.out.println(a-b);
+	}
+}
